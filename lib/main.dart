ad351765ba40630d378cac402686656ebe762450
sokvebol.kol <sokvebol.kol@gmail.com>
2025-09-30 13:49:02 +0700

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -75,6 +75,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Chokchey HR',
       theme: ThemeData(
         fontFamily: 'times',
